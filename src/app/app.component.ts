3fc92716201e79a7ce43b0274270201ef75260b8
Jose Ribeiro <joseribeirocsr@gmail.com>
2024-02-18 21:15:57 -0300

    sistema de ponto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {MarkingComponent} from './marking/marking.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+  HomeComponent,
+MarkingComponent
+  
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pont-register';
+  title = 'Registro de ponto';
 }
---
+++ src/app/marking/marking.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-marking',
+  standalone: true,
+  imports: [],
+  templateUrl: './marking.component.html',
+  styleUrl: './marking.component.css'
+})
+export class MarkingComponent {
+
+}
